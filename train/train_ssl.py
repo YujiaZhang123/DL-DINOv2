ed7db7809efea4690c11978289b6c07a0f0365aa
YujiaZhang123 <157528365+YujiaZhang123@users.noreply.github.com>
2025-11-26 00:34:32 -0500

    Update train_ssl.py

--- train/train_ssl.py
+++ train/train_ssl.py
@@ -44,7 +44,7 @@ class TrainingConfig:
 
     # ----- optimization -----
     batch_size: int = 220             
-    num_workers: int = 26
+    num_workers: int = 24
     epochs: int = 160
     base_lr: float = 2e-4
     min_lr: float = 2e-6
